da4d59009c49a526f11f9fce0ac4841ec746a8c8
Agam Nigam <wrk.agam@gmail.com>
2026-05-28 17:55:21 +0530

    fix(email): resolve smtp timeout issue on render

--- utils/mailer.js
+++ utils/mailer.js
@@ -2,12 +2,26 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASS
+        pass: process.env.EMAIL_PASSWORD
+    },
+
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000
+});
+
+transporter.verify((err, success) => {
+
+    if (err) {
+        console.log("Mail Error:", err);
+    } else {
+        console.log("Mail Server Ready");
     }
+
 });
 
 module.exports = transporter;

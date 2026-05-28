91a1a55b018ab09f1697402b3fa973fa92e15573
Agam Nigam <wrk.agam@gmail.com>
2026-05-28 22:42:09 +0530

    fix: switch to port 2525 to bypass Render SMTP block

--- utils/mailer.js
+++ utils/mailer.js
@@ -2,8 +2,8 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
-    port: 465,
-    secure: true,
+    port: 2525,
+    secure: false,
 
     auth: {
         user: process.env.BREVO_EMAIL,

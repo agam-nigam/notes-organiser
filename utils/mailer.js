84d09750574b0df4e7ea2698c9ece7bd7cd523f7
Agam Nigam <wrk.agam@gmail.com>
2026-05-29 20:23:40 +0530

    feat(email): integrate brevo smtp password reset emails

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,13 +1,43 @@
-const { Resend } = require("resend");
-const resend = new Resend(process.env.RESEND_API_KEY);
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+    host: "smtp-relay.brevo.com",
+    port: 2525,
+    secure: false,
+    auth: {
+        user: process.env.BREVO_EMAIL,
+        pass: process.env.BREVO_SMTP_KEY
+    }
+});
 
 const sendResetEmail = async (to, resetUrl) => {
-    await resend.emails.send({
-        from: "Notes Organiser <onboarding@resend.dev>",
-        to: to,
-        subject: "Password Reset Request",
-        html: `<p>Click <a href="${resetUrl}">here</a> to reset your password. Link expires in 30 minutes.</p>`
+    const info = await transporter.sendMail({
+        from: "Notes Organiser <notesorganiserapp@gmail.com>",
+        to,
+        subject: "Notes Organiser - Password Reset Request",
+        html: `
+            <h2>Password Reset Request</h2>
+            <p>
+                You requested a password reset for your Notes Organiser account.
+            </p>
+            <p>
+                Click the button below to reset your password:
+            </p>
+            <p>
+                <a href="${resetUrl}">
+                    Reset Password
+                </a>
+            </p>
+            <p>
+                This link will expire in 30 minutes.
+            </p>
+            <p>
+                If you did not request this password reset,
+                please ignore this email.
+            </p>
+        `
     });
+    console.log("Email sent:", info.messageId);
 };
 
 module.exports = sendResetEmail;
